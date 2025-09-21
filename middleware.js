148e1526e0d05778ca1d5104eeab87040e229a26
shogo-3345 <s276174@wakayama-u.ac.jp>
2025-09-21 20:55:23 +0900

    auth commit

--- middleware.js
+++ middleware.js
@@ -1,20 +1,31 @@
-// middleware.js
-export function middleware(request) {
-  const auth = request.headers.get('authorization');
+import { NextResponse } from 'next/server';
 
-  // ここでユーザー名とパスワードを設定
-  const username = process.env.BASIC_AUTH_USERNAME || 'badminton44';
-  const password = process.env.BASIC_AUTH_PASSWORD || '267499ssyt';
-  const basicAuth = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+export const config = {
+  matcher: '/(.*)', // すべてのパスを対象にする
+};
 
-  if (auth !== basicAuth) {
-    return new Response('Authentication required', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Protected Area"',
-      },
-    });
+export default function middleware(request) {
+  const authorizationHeader = request.headers.get("authorization");
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).toString().split(":");
+
+    // 環境変数の値と一致するかチェック
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      // 認証成功時は次の処理へ進める
+      return NextResponse.next();
+    }
   }
 
-  return Response.next();
+  // 認証失敗時は401エラーと認証ダイアログを表示するヘッダーを返す
+  return new NextResponse("Authentication required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
 }
